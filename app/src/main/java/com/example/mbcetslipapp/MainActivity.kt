3a0e733fd29fd173b672093f83ab757f7d18ae93
Ben George Netto <bengnetto245@gmail.com>
2022-10-15 01:36:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mbcetslipapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mbcetslipapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mbcetslipapp.ui.theme.MBCETSlipAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MBCETSlipAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MBCETSlipAppTheme {
+
+    }
+}
