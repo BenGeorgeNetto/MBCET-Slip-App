8ccedb0c8695ac6b6d0dcb3bec2f7326da4ea987
Ben George Netto <bengnetto245@gmail.com>
2022-10-16 20:42:19 +0530

    Add Navigation and Bottom Navigation Bar

--- app/src/main/java/com/example/mbcetslipapp/MainActivity.kt
+++ app/src/main/java/com/example/mbcetslipapp/MainActivity.kt
@@ -4,17 +4,25 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.mbcetslipapp.data.AllSlips
 import com.example.mbcetslipapp.screens.*
 import com.example.mbcetslipapp.ui.theme.*
+import java.security.AccessController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    PTrackerAppListstd(ptrackerapplist= AllSlips().loadSlips())
+                    SlipApp()
                 }
             }
         }
@@ -40,14 +48,40 @@ fun SlipApp() {
 
 @Composable
 fun SlipHome() {
-    Scaffold(
-        bottomBar = { BottomNavigationBar() },
-        backgroundColor = MaterialTheme.colors.background,
-        modifier = Modifier
-    ) {
-        IdCard()
-    }
+    val navController = rememberNavController()
 
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) },
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = Modifier,
+        content = { padding ->
+            Box() {
+                Navigation(navController = navController)
+            }
+        }
+    )
+
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            HomeScreenStudent()
+        }
+        composable(NavigationItem.Add.route) {
+            CreateSlipLayout()
+        }
+        composable(NavigationItem.Slips.route) {
+            PermissionScreen()
+        }
+        composable(NavigationItem.Wifi.route) {
+            ItmsWifiSlip()
+        }
+        composable(NavigationItem.ID.route) {
+            IdCard()
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
@@ -58,17 +92,14 @@ fun LightPreview() {
     }
 }
 
-@Composable
-fun TopAppBar() {
-    // to define top app bar
-}
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         NavigationItem.Home,
         NavigationItem.Add,
         NavigationItem.Slips,
+        NavigationItem.Wifi,
         NavigationItem.ID
     )
     BottomNavigation(
@@ -76,16 +107,28 @@ fun BottomNavigationBar() {
         contentColor = Grey1,
         elevation = 4.dp
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
         items.forEach { item ->
             BottomNavigationItem(
                 icon = { item.icon },
                 label = { Text(item.title) },
-                selectedContentColor = PrimGreen,
+                selectedContentColor = PrimBlue,
                 unselectedContentColor = PrimCream,
                 alwaysShowLabel = true,
-                selected = false,
+                selected = currentRoute == item.route,
                 onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
 
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
             )
         }
--- app/src/main/java/com/example/mbcetslipapp/NavigationItem.kt
+++ app/src/main/java/com/example/mbcetslipapp/NavigationItem.kt
@@ -1,15 +1,13 @@
 package com.example.mbcetslipapp
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.Face
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Note
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
     object Home : NavigationItem("home", Icons.Outlined.Home, "Home")
     object Add : NavigationItem("add", Icons.Outlined.Add, "Add")
     object Slips : NavigationItem("slips", Icons.Outlined.Note, "Note")
+    object Wifi: NavigationItem("wifi", Icons.Outlined.Wifi, "Wifi")
     object ID : NavigationItem("id", Icons.Outlined.Face, "ID")
 }
