8f16578f5bbf3c90eac6cc2694fbba401a4d1b2d
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2022-10-16 18:21:31 +0530

    Added Registration.kt

--- app/src/main/java/com/example/mbcetslipapp/MainActivity.kt
+++ app/src/main/java/com/example/mbcetslipapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mbcetslipapp.screens.CreateSlipLayout
 import com.example.mbcetslipapp.screens.IdCard
+import com.example.mbcetslipapp.screens.LoginAndRegistration
 import com.example.mbcetslipapp.screens.PermissionScreen
 import com.example.mbcetslipapp.ui.theme.MBCETSlipAppTheme
 
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    PermissionScreen()
+                    LoginAndRegistration()
                 }
             }
         }
